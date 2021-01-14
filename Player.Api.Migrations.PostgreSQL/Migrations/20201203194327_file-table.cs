3c735b7d1fcb91c957c287fb082a1828d347dea7
Ben Young <72205768+sei-byoung@users.noreply.github.com>
2021-01-14 11:23:53 -0500

    Add headers

--- Player.Api.Migrations.PostgreSQL/Migrations/20201203194327_file-table.cs
+++ Player.Api.Migrations.PostgreSQL/Migrations/20201203194327_file-table.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright 2021 Carnegie Mellon University. All Rights Reserved.
+// Released under a MIT (SEI)-style license. See LICENSE.md in the project root for license information.
+
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
