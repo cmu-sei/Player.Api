70ccfd3cd3557071c4021e0bf0ebf82d22a0a678
Eric Bram <61522167+sei-ebram@users.noreply.github.com>
2021-02-10 14:19:52 -0500

    add error

--- Player.Api/Services/PermissionService.cs
+++ Player.Api/Services/PermissionService.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
-using System.Security.Principal;
+uaaaasing System.Security.Principal;
 using System.Threading.Tasks;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
