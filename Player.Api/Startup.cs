c65434a2fbbb53cd0d4146cc2b088b1b21ebab56
sei-noconnor <72139157+sei-noconnor@users.noreply.github.com>
2021-10-14 10:29:13 -0400

    bugfix: swagger endpoint uses pathbase variable (#23)

--- Player.Api/Startup.cs
+++ Player.Api/Startup.cs
@@ -1,35 +1,35 @@
 // Copyright 2021 Carnegie Mellon University. All Rights Reserved.
 // Released under a MIT (SEI)-style license. See LICENSE.md in the project root for license information.
 
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Principal;
+using System.Text.Json.Serialization;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
-using Player.Api.Extensions;
 using Player.Api.Data.Data;
+using Player.Api.Extensions;
+using Player.Api.Infrastructure.Authorization;
+using Player.Api.Infrastructure.ClaimsTransformers;
+using Player.Api.Infrastructure.Extensions;
+using Player.Api.Infrastructure.Filters;
+using Player.Api.Infrastructure.Mappings;
 using Player.Api.Options;
 using Player.Api.Services;
-using System;
-using System.Text.Json.Serialization;
-using AutoMapper;
-using Player.Api.Infrastructure.Filters;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Principal;
-using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Authentication;
-using Player.Api.Infrastructure.ClaimsTransformers;
-using Microsoft.AspNetCore.Authorization;
-using System.Linq;
-using Player.Api.Infrastructure.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Player.Api.Infrastructure.Extensions;
-using Player.Api.Infrastructure.Mappings;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace Player.Api
 {
@@ -192,7 +192,7 @@ namespace Player.Api
             app.UseSwaggerUI(c =>
             {
                 c.RoutePrefix = _routePrefix;
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Player v1");
+                c.SwaggerEndpoint($"{_pathbase}/swagger/v1/swagger.json", "Player v1");
                 c.OAuthClientId(_authOptions.ClientId);
                 c.OAuthClientSecret(_authOptions.ClientSecret);
                 c.OAuthAppName(_authOptions.ClientName);
