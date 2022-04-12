e38005a9560b935a973b62976f70daa16aba21be
Tim Spencer <72101647+sei-tspencer@users.noreply.github.com>
2022-04-12 14:07:54 -0400

    added azure application insights option (#35)

--- Player.Api/Startup.cs
+++ Player.Api/Startup.cs
@@ -49,6 +49,13 @@ namespace Player.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add Azure Application Insights, if connection string is supplied
+            string appInsights = Configuration["ApplicationInsights:ConnectionString"];
+            if (!string.IsNullOrWhiteSpace(appInsights))
+            {
+                services.AddApplicationInsightsTelemetry();
+            }
+
             var provider = Configuration["Database:Provider"];
             switch (provider)
             {
