c4aa611632a2ef0dc7d204e51c53c62f1c3c60d6
Andrew Schlackman <72105194+sei-aschlackman@users.noreply.github.com>
2025-03-19 13:53:51 -0400

    adds RevertVms permission for vm.api (#46)

---
+++ Player.Api.Migrations.PostgreSQL/Migrations/20250317143056_RevertVms_Permission.cs
@@ -0,0 +1,32 @@
+// Copyright 2025 Carnegie Mellon University. All Rights Reserved.
+// Released under a MIT (SEI)-style license. See LICENSE.md in the project root for license information.
+
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Player.Api.Migrations.PostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class RevertVms_Permission : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "team_permissions",
+                columns: new[] { "id", "description", "immutable", "name" },
+                values: new object[] { new Guid("42da22ae-ca0f-440f-87e0-5742799f60e1"), "Allows reverting a Vm to it's current snapshot", false, "RevertVms" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "team_permissions",
+                keyColumn: "id",
+                keyValue: new Guid("42da22ae-ca0f-440f-87e0-5742799f60e1"));
+        }
+    }
+}
