ecba058f19c2078c6881d5c1c2bdd1f4a26f6329
Andrew Schlackman <72105194+sei-aschlackman@users.noreply.github.com>
2025-06-27 16:31:13 -0400

    wipwip

--- Player.Api/Program.cs
+++ Player.Api/Program.cs
@@ -1,6 +1,7 @@
 // Copyright 2022 Carnegie Mellon University. All Rights Reserved.
 // Released under a MIT (SEI)-style license. See LICENSE.md in the project root for license information.
 
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
@@ -10,13 +11,17 @@ namespace Player.Api;
 
 public class Program
 {
-        public static void Main(string[] args)
+    public static void Main(string[] args)
+    {
+        var host = CreateWebHostBuilder(args).Build();
+
+        if (!args.Contains("--no-db"))
         {
-            CreateWebHostBuilder(args)
-                .Build()
-                .InitializeDatabase()
-                .Run();
+            host = host.InitializeDatabase();
         }
+
+        host.Run();
+    }
 
     public static IHostBuilder CreateWebHostBuilder(string[] args)
     {
