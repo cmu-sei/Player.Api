cad58783675ad1b286053dcd86d3b33c2418ed52
Jarrett Booz <jabooz@sei.cmu.edu>
2025-10-31 17:56:17 +0000

    Rename telemetry service to player-api
    - This prevents some metrics from showing up under "TelemetryService" while others use "player-api"

--- Player.Api/Startup.cs
+++ Player.Api/Startup.cs
@@ -218,7 +218,7 @@ public class Startup
             .WithMetrics(builder =>
             {
                 builder
-                    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("TelemetryService"))
+                    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("player-api"))
                     .AddMeter
                     (
                         TelemetryService.ViewUsersMeterName
