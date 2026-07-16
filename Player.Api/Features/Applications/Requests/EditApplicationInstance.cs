274032a26018df94218e8ba1cf3ac5c5df64a659
Andrew Schlackman <72105194+sei-aschlackman@users.noreply.github.com>
2026-07-16 19:25:16 +0000

    fix build

--- Player.Api/Features/Applications/Requests/EditApplicationInstance.cs
+++ Player.Api/Features/Applications/Requests/EditApplicationInstance.cs
@@ -2,6 +2,7 @@
 // Released under a MIT (SEI)-style license. See LICENSE.md in the project root for license information.
 
 using System;
+using System.Linq;
 using System.Runtime.Serialization;
 using System.Text.Json.Serialization;
 using System.Threading;
