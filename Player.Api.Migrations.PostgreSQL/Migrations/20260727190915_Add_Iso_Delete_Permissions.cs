30d2c95bf1e10838830d89c27a0d169a0c225d8b
Andrew Schlackman <72105194+sei-aschlackman@users.noreply.github.com>
2026-07-27 20:01:59 +0000

    fix merge

--- Player.Api.Migrations.PostgreSQL/Migrations/20260727190915_Add_Iso_Delete_Permissions.cs
+++ Player.Api.Migrations.PostgreSQL/Migrations/20260727190915_Add_Iso_Delete_Permissions.cs
@@ -11,11 +11,16 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Player.Api.Migrations.PostgreSQL.Migrations
 {
     /// <inheritdoc />
-    public partial class Delete_Iso_Permissions : Migration
+    public partial class Add_Iso_Delete_Permissions : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.InsertData(
+                table: "permissions",
+                columns: new[] { "id", "description", "immutable", "name" },
+                values: new object[] { new Guid("0189a1b9-d975-4f14-b93c-db652f7dc064"), "Allows deleting any ISO across all Views", false, "DeleteIsos" });
+
             migrationBuilder.InsertData(
                 table: "team_permissions",
                 columns: new[] { "id", "description", "immutable", "name" },
@@ -29,6 +34,11 @@ namespace Player.Api.Migrations.PostgreSQL.Migrations
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DeleteData(
+                table: "permissions",
+                keyColumn: "id",
+                keyValue: new Guid("0189a1b9-d975-4f14-b93c-db652f7dc064"));
+
             migrationBuilder.DeleteData(
                 table: "team_permissions",
                 keyColumn: "id",
